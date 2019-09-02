5cb4c8cbf3123a3ff29963aacfb8ab6df33db561
Adrian Lopez <adrianlrx@gmail.com>
2019-09-02 14:37:06 -0400

    controllers

--- src/controllers/tareas.controller.js
+++ src/controllers/tareas.controller.js
@@ -1,9 +1,42 @@
 const tareaController = {};
 
-tareaController.getTareas = (req,res)=>{};
-tareaController.getTarea = (req,res)=>{};
-tareaController.createTarea = (req,res)=>{};
-tareaController.updateTarea = (req,res)=>{};
-tareaController.deleteTarea = (req,res)=>{};
+/** view task list */
+tareaController.getTareas = (req,res)=>{
+    res.json({
+        message: 'Listando tareas'
+    });
+};
+
+/** view task */
+tareaController.getTarea = (req,res)=>{
+    res.json({
+        message: 'Ver tarea',
+        idTarea: req.param.id
+    });
+};
+
+/**create task */
+tareaController.createTarea = (req,res)=>{
+    res.json({
+        message: 'Creando tareas',
+        idTarea: req.param.id
+    });
+};
+
+/** update task */
+tareaController.updateTarea = (req,res)=>{
+    res.json({
+        message: 'Actualizando tareas',
+        idTarea: req.param.id
+    });
+};
+
+/** delete task */
+tareaController.deleteTarea = (req,res)=>{
+    res.json({
+        message: 'Eliminando tareas',
+        idTarea: req.param.id
+    });
+};
 
 module.exports = tareaController;
