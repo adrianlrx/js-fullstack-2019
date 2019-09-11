c28b2e39896d40abef47e023230910cf59159341
Adrian Lopez <adrianlrx@gmail.com>
2019-09-10 22:22:04 -0400

    database conf

--- src/database.js
+++ src/database.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/tareadb', {useNewUrlParser: true})
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+})
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(er));
